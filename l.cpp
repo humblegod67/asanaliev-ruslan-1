a14b254731881a8f4e6255425879683827c7cfa4
humblegod67 <151152304+humblegod67@users.noreply.github.com>
2023-11-17 13:59:23 +0300

    1

---
+++ l.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+
+{
+	setlocale(0, "");
+	cout << "Введите число" << endl;
+	int n;
+	cin >> n;
+	int t = 0;
+	cout << n << " в квадрате=";
+	cout << n * n << endl;
+	cout << n << " в кубе=";
+	n / n;
+	cout << n * n * n << endl;
+
+}
